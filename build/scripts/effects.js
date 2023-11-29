d68cbd3a117ae6a15ff6eed7686f423ce392b8d6
DaniVegaM <dany.17.com@hotmail.com>
2023-11-28 23:22:06 -0600

    Efecto de sidebar agregado NO responsive aun

--- build/scripts/effects.js
+++ build/scripts/effects.js
@@ -57,3 +57,14 @@ switchBtn.addEventListener("click", event =>{
         }
     }
 });
+
+//SideBar
+
+const sidebar = document.querySelector(".sidebar");
+const toggle = document.querySelector(".toggle");
+
+toggle.addEventListener("click", ()=>{
+    sidebar.classList.toggle("close");
+    console.log("Clic en sidebar");
+});
+
